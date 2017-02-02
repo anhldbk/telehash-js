374a5f1048344803a8a8dd7a7f095378d4d93d52
Anh Le <anhldbk@gmail.com>
2017-02-02 16:08:32 +0700

    Fixed errors in examples/stream.js
    **Problem:** The example does NOT work well. The code is synchronously implemented.
    
    **Solution**: Implementing code asynchronously.

--- examples/stream.js
+++ examples/stream.js
@@ -1,22 +1,90 @@
-var es = require('event-stream');
-var telehash = require('..');
+var es = require('event-stream'),
+  telehash = require('telehash'),
+  EventEmitter = require('events').EventEmitter,
+  trigger = new EventEmitter(),
+  meshA,
+  meshB,
+  confA = {
+    id: {
+      "keys": {
+        "1a": "akndnx5kbansip6xphxymwckpqkjj26fcm"
+      },
+      "secrets": {
+        "1a": "ksxslm5mmtymnbph7nvxergb7oy3r35u"
+      },
+      "hashname": "5uegloufcyvnf34jausszmsabbfbcrg6fyxpcqzhddqxeefuapvq"
+    }
+  },
+  confB = {
+    id: {
+      "keys": {
+        "1a": "apkoh54rkobkeela6d62hblhqd7grqd5dm"
+      },
+      "secrets": {
+        "1a": "ljfm3ov42x2fl6gsg6bxiqgtstnxls7r"
+      },
+      "hashname": "fvifxlr3bsaan2jajo5qqn4au5ldy2ypiweazmuwjtgtg43tirkq"
+    }
+  };
 
-var meshA = telehash.mesh({id:{"keys":{"1a":"akndnx5kbansip6xphxymwckpqkjj26fcm"},"secrets":{"1a":"ksxslm5mmtymnbph7nvxergb7oy3r35u"},"hashname":"5uegloufcyvnf34jausszmsabbfbcrg6fyxpcqzhddqxeefuapvq"}});
-
-var meshB = telehash.mesh({id:{"keys":{"1a":"apkoh54rkobkeela6d62hblhqd7grqd5dm"},"secrets":{"1a":"ljfm3ov42x2fl6gsg6bxiqgtstnxls7r"},"hashname":"fvifxlr3bsaan2jajo5qqn4au5ldy2ypiweazmuwjtgtg43tirkq"}});
-
-// connect them to each other (for same-process testing only)
-meshA.mesh(meshB);
-
-// accept a stream of items
-meshB.stream(function(link, req, accept){
-  var streamBA = accept();
-  streamBA.pipe(es.writeArray(function(err,items){
-    console.log(items);
-    process.exit(0);
-  }));
+trigger.on('create-meshA', () => {
+  telehash.mesh(confA, (err, mesh) => {
+    if (err) {
+      return console.log('!> Error in creating mesh A');
+    }
+    console.log('!> Mesh A created successfully.');
+    meshA = mesh;
+    trigger.emit('create-meshB');
+  });
 })
 
-// stream objects from A to B
-var streamAB = meshA.link(meshB.hashname).stream();
-es.readArray([1,2,true,{all:42}]).pipe(streamAB);
+trigger.on('create-meshB', () => {
+  telehash.mesh(confB, (err, mesh) => {
+    if (err) {
+      return console.log('!> Error in creating mesh B');
+    }
+    console.log('!> Mesh B created successfully.');
+    meshB = mesh;
+    trigger.emit('connect-meshes');
+  });
+});
+
+trigger.on('connect-meshes', () => {
+  console.log("!> Connecting two meshes (for same-process testing only)...")
+  meshA.mesh(meshB)
+    .then(
+      (success) => {
+        console.log('!> Two meshes are connected successfully.');
+        trigger.emit('simulate');
+      },
+      (failed) => {
+        console.log('!> Failed to connect two meshes');
+      }
+    );
+});
+
+trigger.on('simulate', () => {
+  console.log('!> Simulating...');
+
+  // accept a stream of items
+  meshB.stream(function (link, req, accept) {
+    var streamBA = accept();
+    streamBA.pipe(es.writeArray(function (err, items) {
+      console.log('!> Receiving items...');
+      console.log(items);
+      process.exit(0);
+    }));
+  })
+
+  // stream objects from A to B
+  var streamAB = meshA
+    .link(meshB.hashname)
+    .stream();
+  console.log('!> Sending items...');
+  es.readArray([1, 2, true, {
+    all: 42
+  }]).pipe(streamAB);
+});
+
+// start
+trigger.emit('create-meshA');
